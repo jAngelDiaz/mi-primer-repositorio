e70a523a251538e72eb75745b087c92a58302ddd
jAngelDiaz <jose3009diaz@gmail.com>
2022-12-13 14:23:15 -0400

    iniciando mi repositorio

---
+++ prueba.vue
@@ -0,0 +1,22 @@
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+<script>
+
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
